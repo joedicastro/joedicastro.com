c219e38165581f16fc039e0e9176ad1a23f3209d
joe di castro <joe@joedicastro.com>
2013-02-08 20:09:16 +0100

    Mejor cumplimiento del PEP-8 y actualizar htaccess

--- site/pelican.conf.py
+++ site/pelican.conf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-#===============================================================================
+#==============================================================================
 # PELICAN SETTINGS FILE
-#===============================================================================
+#==============================================================================
 
 # Add personalized 404 & 403 error pages to direct templates
 DIRECT_TEMPLATES = ('index', 'tags', 'archives', 'notfound', 'access_denied')
@@ -186,8 +186,10 @@ FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),
                  ('extra/favicon.ico', 'favicon.ico'),
                  ('extra/apple-touch-icon.png', 'apple-touch-icon.png'),
                  ('extra/.htaccess', '.htaccess'),
-                 ('extra/google8d74e9875d1491cf.html', 'google8d74e9875d1491cf.html'),
-                 ('extra/y_key_99d30f68509828bf.html', 'y_key_99d30f68509828bf.html'))
+                 ('extra/google8d74e9875d1491cf.html',
+                  'google8d74e9875d1491cf.html'),
+                 ('extra/y_key_99d30f68509828bf.html',
+                  'y_key_99d30f68509828bf.html'))
 
 # foobar will not be used, because it's not in caps. All configuration keys
 # have to be in caps
